9418378a7db80d7034ecf5ca5440d2f132cc85eb
lithid <long.jeremie@gmail.com>
2011-03-22 23:37:32 -0400

    Add LCD Density adjustment via SalvageParts

--- src/com/salvagemod/salvageparts/activities/UIActivity.java
+++ src/com/salvagemod/salvageparts/activities/UIActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.salvagemod.salvageparts.activities;
 
@@ -8,6 +23,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcel;
+import android.os.SystemProperties;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.preference.CheckBoxPreference;
@@ -29,12 +45,16 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
     /* Other */
 
     private static final String OVERSCROLL_PREF = "pref_overscroll_effect";
-
     private static final String OVERSCROLL_WEIGHT_PREF = "pref_overscroll_weight";
+    private static final String LCDD_PREF = "pref_lcdd";
+    private static final String LCDD_PROP = "ro.sf.lcd_density";
+    private static final String LCDD_PERSIST_PROP = "persist.sys.lcd_density";
+    private static final String LCDD_DEFAULT = "240";
 
     private ListPreference mOverscrollPref;
-
     private ListPreference mOverscrollWeightPref;
+   
+    private ListPreference mLcddPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,15 +80,29 @@ public class UIActivity extends PreferenceActivity implements OnPreferenceChange
         mOverscrollWeightPref.setValue(String.valueOf(overscrollWeight));
         mOverscrollWeightPref.setOnPreferenceChangeListener(this);
 
+   	/* LCD Density Changer */
+	mLcddPref = (ListPreference) prefSet.findPreference(LCDD_PREF);
+        mLcddPref.setValue(SystemProperties.get(LCDD_PROP,
+                SystemProperties.get(LCDD_PERSIST_PROP, LCDD_DEFAULT)));
+        mLcddPref.setOnPreferenceChangeListener(this); 
+
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLcddPref) {
+            if (newValue != null) {
+                SystemProperties.set(LCDD_PERSIST_PROP, (String)newValue);
+        return true;
+	    }
+        }
 	if (preference == mOverscrollPref) {
             int overscrollEffect = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.OVERSCROLL_EFFECT,
                     overscrollEffect);
             return true;
-        } else if (preference == mOverscrollWeightPref) {
+        }
+        if (preference == mOverscrollWeightPref) {
             int overscrollWeight = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.OVERSCROLL_WEIGHT,
                     overscrollWeight);
